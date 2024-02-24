ea3b2a88eda363204e7100537c182896230c217c
野良猫しぃ <neko@noranekofactory.red>
2024-02-24 17:50:29 +0900

    result

--- src/app/routes/title.js
+++ src/app/routes/title.js
@@ -1,32 +1,31 @@
-const express = require('express');
-const { VALIDATOR: V } = require('../../common/validateParams');
+const express = require("express");
+const { VALIDATOR: V } = require("../../common/validateParams");
 const router = express.Router();
-const { doRoute } = require('../../common/routes/base/routeBase');
-const db = require('../../common/persistence/simple');
-const { insertTitle, getTitles } = require('../persistence/title');
+const { doRoute } = require("../../common/routes/base/routeBase");
+const db = require("../../common/persistence/simple");
+const { insertTitle, getTitles } = require("../persistence/title");
 
 /** get test */
-router.get('/get',
-  async function(req, res) {
-    doRoute(req, res, async (r, auth) => {
-      const titleList = await getTitles();
-      // TEST
-      return {
-        result: 0,
-        titleList,
-      };
-    });
-  }
-);
+router.get("/get", async function (req, res) {
+  doRoute(req, res, async (r, auth) => {
+    const titleList = await getTitles();
+    // TEST
+    return {
+      result: 0,
+      titleList,
+    };
+  });
+});
 
 /** post test */
-router.post('/post',
+router.post(
+  "/post",
   [
-   V.anyLengthSanitized('titleName', 256, true),
-   V.anyLengthSanitized('overview', 256, true),
-   V.anyLengthSanitized('genre', 256, true),
+    V.anyLengthSanitized("titleName", 256, true),
+    V.anyLengthSanitized("overview", 256, true),
+    V.anyLengthSanitized("genre", 256, true),
   ],
-  async function(req, res) {
+  async function (req, res) {
     doRoute(req, res, async () => {
       const { titleName, overview, genre } = req.body;
       const connection = await db.getConnection();
@@ -38,10 +37,12 @@ router.post('/post',
         await db.rollbackTransaction(connection);
         throw err;
       }
+      return {
+        result: 0,
+      };
     });
   }
 );
-
 
 module.exports = {
   router,
